837139fdac5750450cd4e77ba6940a0f65d89b4a
SotaNorsu <pelaaja538@gmail.com>
2024-01-23 17:01:47 +0200

    edit

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 
 
 
+const queryString =  window.location.search;
+console.log(queryString)
+
+const urlParams = new URLSearchParams(queryString);
+
+const redir = urlParams.get("redir")
+console.log("redir")
+
 function goto() {
     window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
 }
 
-setTimeout(goto ,30000);
+setInterval(goto, 30000);
+
+
