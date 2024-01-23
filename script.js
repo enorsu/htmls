43ec3913e4cdb0625bd5d67eaec83d3eb53b7ab2
SotaNorsu <pelaaja538@gmail.com>
2024-01-23 18:05:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const urlParams = new URLSearchParams(queryString);
 
 const redir = urlParams.get("r");
 
-if (redir == "FREDDY_FAZBE>R_six") {window.location = "log-in84762"}
+if (queryString == "?r=FREDDY_FAZBER_six") {window.location = "log-in84762"}
 
 function goto() {
     window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
