b9139732470530310a7208b60cf299db8cbcfc9a
SotaNorsu <pelaaja538@gmail.com>
2024-01-24 16:24:06 +0200

    Update files

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ const urlParams = new URLSearchParams(queryString);
 
 const redir = urlParams.get("r");
 
+const answer = "123456789"
+
 if (queryString == "?r=FREDDY_FAZBER_six") {window.location = "log-in84762"}
 
 function goto() {
-    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
+    var secretCode = document.getElementById("pwd").value;
+    if(secretCode == answer) {
+        window.location = "log-in84762";
+    } else {
+        window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
+    }
+    
 }
 
 setInterval(goto, 30000);
